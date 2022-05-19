0c44208f4a9b358745f25b6b749df94e12e2429f
Josh Farr <josh@farrcraft.com>
2022-05-18 23:05:14 -0700

    comments

--- Odyssey/config/Config.h
+++ Odyssey/config/Config.h
@@ -15,6 +15,7 @@
 
 namespace odyssey::config {
 	/**
+	 * JSON file-derived configuration
 	 **/
 	class Config final {
 	public:
@@ -23,11 +24,21 @@ namespace odyssey::config {
 		Config(const boost::shared_ptr<odyssey::engine::Logger> &logger);
 
 		/**
+		 * Load all of the standard config
+		 * 
+		 * @param assetManager loads JSON files as managed assets
+		 * 
+		 * @return true if all of the config is successfully loaded
 		 **/
 		bool load(const boost::shared_ptr<odyssey::asset::Manager>& assetManager);
 		
 	protected:
 		/**
+		 * Load the bindings.json config
+		 * 
+		 * @param bindings the json file asset to load bindings from
+		 * 
+		 * @return true if bindings are successfully loaded
 		 **/
 		bool loadBindings(const boost::shared_ptr<odyssey::asset::Json>& bindings);
 
